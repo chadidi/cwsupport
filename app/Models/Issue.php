1e7f1698f05e21637f7ac7db694cd0cc96f75eec
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 18:47:58 +0100

    Extend Customer package classes

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -2,27 +2,8 @@
 
 namespace App\Models;
 
-use App\Traits\HasTags;
-use Illuminate\Database\Eloquent\Model;
+use cwsupport\Customer\Models\Issue as CwsupportIssue;
 
-class Issue extends Model
+class Issue extends CwsupportIssue
 {
-    use HasTags;
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'user_id',
-        'title',
-        'description',
-        'status',
-        'tags',
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,67 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Str;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use cwsupport\Customer\Models\Tag as CwsupportTag;
 
-class Tag extends Model
+class Tag extends CwsupportTag
 {
-    protected $fillable = [
-        'name',
-        'slug',
-    ];
-
-    public function user() : BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function issues() : MorphToMany
-    {
-        return $this->morphedByMany(Issue::class, 'taggable');
-    }
-
-    /**
-     * @param string|array|\ArrayAccess $values
-     * @param string|null $type
-     * @param string|null $locale
-     *
-     * @return Tag|static
-     */
-    public static function findOrCreate($values)
-    {
-        $tags = collect($values)->map(function ($value) {
-            if ($value instanceof self) {
-                return $value;
-            }
-
-            return static::findOrCreateFromString($value);
-        });
-
-        return is_string($values) ? $tags->first() : $tags;
-    }
-
-    public static function findFromSlug(string $slug)
-    {
-        return static::query()
-            ->where('slug', $slug)
-            ->first();
-    }
-    
-    protected static function findOrCreateFromString(string $name)
-    {
-        $slug = Str::slug($name);
-        $tag = static::findFromSlug($slug);
-
-        if (! $tag) {
-            $tag = static::create([
-                'name' => $name,
-                'slug' => $slug,
-            ]);
-        }
-
-        return $tag;
-    }
 }
