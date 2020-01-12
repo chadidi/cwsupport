104895b14cf9b94af73587748247c65bea99d069
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 18:44:53 +0100

    Move user model to Admin package

---
+++ packages/cwsupport/Admin/src/Models/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace cwsupport\Admin\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_admin',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        // 'is_admin' => 'boolean',
+    ];
+
+    // hash a password before saving
+    public function setPasswordAttribute($password)
+    {
+        // Only accept a valid password
+        if ($password !== null & $password !== "") {
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
+}
