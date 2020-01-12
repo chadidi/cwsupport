b37235daad8628966e210a03dfe8e4946e6d38b3
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 18:48:10 +0100

    Extend Admin User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,57 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
+use cwsupport\Admin\Models\User as CwsupportUser;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends CwsupportUser
 {
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'is_admin',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        // 'is_admin' => 'boolean',
-    ];
-
     public function issues() : HasMany
     {
         return $this->hasMany(Issue::class);
-    }
-
-    // hash a password before saving
-    public function setPasswordAttribute($password)
-    {
-        // Only accept a valid password
-        if ($password !== null & $password !== "") {
-            $this->attributes['password'] = bcrypt($password);
-        }
     }
 }
