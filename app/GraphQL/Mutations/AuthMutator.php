d01c52571f91a3fbd0cb04ff5176d10d60324b1c
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 16:58:13 +0100

    Update login method

--- app/GraphQL/Mutations/AuthMutator.php
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -3,42 +3,34 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\User;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class AuthMutator
 {
-    /**
-     * Return a value for the field.
-     *
-     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
-     * @param  mixed[]  $args The arguments that were passed into the field.
-     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
-     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
-     * @return mixed
-     */
     public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        $credentials = Arr::only($args, ['email', 'password']);
+        $user = User::where('email', $args['email'])->first();
 
-        if (Auth::once($credentials)) {
-            $token = Str::random(60);
-
-            $user = auth()->user();
-            $user->api_token = $token;
-            $user->save();
-
-            return [
-                'token_type' => 'Bearer',
-                'access_token' => $token,
-                'user' => $user,
-            ];
+        if (!$user) {
+            return null;
         }
 
-        return null;
+        if (!Hash::check($args['password'], $user->password)) {
+            return null;
+        }
+
+        $token = Str::random(60);
+        $user->api_token = $token;
+        $user->save();
+
+        return [
+            'token_type' => 'Bearer',
+            'access_token' => $token,
+            'user' => $user,
+        ];
     }
 
     public function register($rootValue, array $args, GraphQLContext $context)
