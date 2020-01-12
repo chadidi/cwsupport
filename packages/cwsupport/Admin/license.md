c967d7e2fd5a3cca3d9348faa51bfabd6bf9ed3e
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 18:08:47 +0100

    Create Admin package

---
+++ packages/cwsupport/Admin/license.md
@@ -0,0 +1,5 @@
+# The license
+
+Copyright (c) Abdellah Chadidi <chadidi97@gmail.com>
+
+...Add your license text here...
---
+++ packages/cwsupport/Admin/src/AdminServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace cwsupport\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'cwsupport');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'cwsupport');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/admin.php', 'admin');
+
+        // Register the service the package provides.
+        $this->app->singleton('admin', function ($app) {
+            return new Admin;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['admin'];
+    }
+    
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole()
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/admin.php' => config_path('admin.php'),
+        ], 'admin.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/cwsupport'),
+        ], 'admin.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/cwsupport'),
+        ], 'admin.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/cwsupport'),
+        ], 'admin.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
